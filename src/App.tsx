3b1b23639bf9e875eddb732e994d6d9458e78586
PurpleYamoto <StanislauAstashenka@protonmail.com>
2022-12-21 19:42:15 +0300

    Interface , Product & Products

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-
+import {Product} from './components/Product';
+import { Products } from './data/Products';
 function App() {
   return (
-    <></>
+    <div className='container mx-auto max-w-2xl pt-5'>
+      <Product product={Products[0]}/>
+      <Product product={Products[1]}/>
+    </div>
   );
 }
 
