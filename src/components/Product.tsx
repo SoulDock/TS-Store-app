a5066348cbc0116f1069fcb56e43112d3cd9a4b5
PurpleYamoto <StanislauAstashenka@protonmail.com>
2022-12-21 21:45:27 +0300

    Show/Hide buttons

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IProduct } from '../models'
 
 
@@ -6,9 +6,23 @@ interface ProductProps {
   product: IProduct
 }
 
-export function Product(props :ProductProps) {
+export function Product({product} :ProductProps) {
+  const [details, setDetails] = useState(false)
+
+  const buttonClassName = details ? 'bg-yellow-400 hover:bg-yellow-500' : 'bg-blue-500 hover:bg-blue-700';
+  const buttonClasses = ['text-white font-bold py-2 px-4 rounded mt-2', buttonClassName]
+
   return (
-    <div className='border py-2 px-2 rounded flex flex-col items-center mb-2'>{props.product.title}</div>
+    <div className='border py-2 px-2 rounded flex flex-col items-center mb-2'>
+      <img className='w-1/6' src={product.image} alt={product.title} />
+      {product.title}
+      <span className='font-bold'>{product.price}</span>
+      {details && <div>{product.description}</div>}
+      <button className={buttonClasses.join(' ')} 
+        onClick={()=> setDetails(prev => !prev)}>
+        {details ? 'Hide Details' : 'Show details'}
+      </button>
+      </div>
   )
 }
 
